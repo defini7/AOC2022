6b5d4fd6b661b058737794ea75b586e50686abfc
Алекс <75216201+defini7@users.noreply.github.com>
2022-12-07 15:48:26 +0300

    Create Part2.cpp

---
+++ day6/Part2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdint>
+#include <algorithm>
+
+int main()
+{
+    std::ifstream file("input.txt");
+	if (!file.is_open()) return 1;
+
+    std::string sLine;
+    std::getline(file, sLine);
+
+    size_t nProcessedChars = 0;
+    for (size_t i = 0; i < sLine.length(); i++)
+    {
+        std::string sMsg = std::string(sLine.begin() + i, sLine.begin() + i + 14);
+
+        size_t nValidChars = 0;
+        for (auto& c : sMsg)
+        {
+            if (std::count(sMsg.begin(), sMsg.end(), c) == 1)
+                nValidChars++;
+        }
+
+        if (nValidChars == 14)
+        {
+            nProcessedChars = i + 14;
+            break;
+        }
+
+    }
+
+    std::cout << nProcessedChars << std::endl;
+
+    return 0;
+}
