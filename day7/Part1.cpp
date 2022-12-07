6050558ea082b4da7a2a9eb2d95d7c4620460392
Алекс <75216201+defini7@users.noreply.github.com>
2022-12-07 16:41:26 +0300

    Create Part1.cpp

---
+++ day7/Part1.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+
+std::vector<std::string> Split(std::vector<std::string>& vecOutput, std::string sInput, std::string sDelimiter)
+{
+	size_t nStart = 0;
+	size_t nEnd;
+
+	vecOutput.clear();
+	while ((nEnd = sInput.find(sDelimiter, nStart)) != std::string::npos)
+	{
+		vecOutput.push_back(sInput.substr(nStart, nEnd - nStart));
+		nStart = nEnd + sDelimiter.length();
+	}
+	vecOutput.push_back(sInput.substr(nStart));
+
+	return vecOutput;
+}
+
+int main()
+{
+	std::ifstream file("input.txt");
+	if (!file.is_open()) return 1;
+
+	std::unordered_map<std::string, size_t> mapFileSystem;
+	std::vector<std::string> vecPath;
+
+	while (!file.eof())
+	{
+		std::string sLine;
+		std::getline(file, sLine);
+		
+		std::vector<std::string> vecLine;
+		Split(vecLine, sLine, " ");
+
+		if (vecLine[1] == "cd")
+		{
+			if (vecLine[2] == "..")
+				vecPath.pop_back();
+			else
+				vecPath.push_back(vecLine[2]);
+		}
+		else if (vecLine[1] == "ls")
+		{
+			continue;
+		}
+		else
+		{
+			try
+			{
+				size_t nSize = std::stoul(vecLine[0]);
+				
+				for (size_t i = 0; i < vecPath.size() + 1; i++)
+				{
+					std::string sPath = "";
+					for (size_t j = 0; j < i; j++)
+						sPath.append(vecPath[j]);
+
+					mapFileSystem[sPath] += nSize;
+				}
+
+			}
+			catch (...) {}
+		}
+
+	}
+
+	size_t nTotal = 0;
+
+	for (const auto& e : mapFileSystem)
+	{
+		if (e.second <= 100000)
+			nTotal += e.second;
+	}
+
+	std::cout << nTotal << std::endl;
+
+	return 0;
+}
