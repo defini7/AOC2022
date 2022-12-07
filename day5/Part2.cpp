6ae73b2b9f91073991b47004e809119449eaf98b
Алекс <75216201+defini7@users.noreply.github.com>
2022-12-07 15:08:02 +0300

    Create Part2.cpp

---
+++ day5/Part2.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdint>
+#include <deque>
+#include <stack>
+#include <list>
+
+int main()
+{
+    std::ifstream file("input.txt");
+	if (!file.is_open()) return 1;
+
+    std::stack<char> stkStacks[9];
+
+    // Cheat a little bit
+    stkStacks[0] = std::stack(std::deque<char>({
+        'T', 'P', 'Z', 'C',
+        'S', 'L', 'Q', 'N'
+    }));
+
+    stkStacks[1] = std::stack(std::deque<char>({
+        'L', 'P', 'T', 'V',
+        'H', 'C', 'G'
+    }));
+
+    stkStacks[2] = std::stack(std::deque<char>({
+        'D', 'C', 'F', 'Z',
+    }));
+
+    stkStacks[3] = std::stack(std::deque<char>({
+        'G', 'W', 'T', 'D',
+        'L', 'M', 'V', 'C'
+    }));
+
+    stkStacks[4] = std::stack(std::deque<char>({
+        'P', 'W', 'C'
+    }));
+
+    stkStacks[5] = std::stack(std::deque<char>({
+        'P', 'F', 'J', 'D',
+        'C', 'T', 'S', 'Z'
+    }));
+
+    stkStacks[6] = std::stack(std::deque<char>({
+        'V', 'W', 'G', 'B', 'D'
+    }));
+
+    stkStacks[7] = std::stack(std::deque<char>({
+        'N', 'J', 'S', 'Q', 'H', 'W'
+    }));
+
+    stkStacks[8] = std::stack(std::deque<char>({
+        'R', 'C', 'Q', 'F', 'S', 'L', 'V'
+    }));
+
+    while (!file.eof())
+    {
+        std::string sLine;
+        std::getline(file, sLine);
+
+        size_t nAfterMove = sLine.find("move") + 5;
+        size_t nCount = std::stoul(sLine.substr(nAfterMove, sLine.find_first_of(' ', nAfterMove) - 1));
+
+        size_t nAfterFrom = sLine.find("from") + 5;
+        size_t nFrom = std::stoul(sLine.substr(nAfterFrom, sLine.find_first_of(' ', nAfterFrom) - 1));
+
+        size_t nAfterTo = sLine.find("to") + 3;
+        size_t nTo = std::stoul(sLine.substr(nAfterTo, sLine.find_first_of(' ', nAfterTo) - 1));
+
+        std::list<char> listBuffer;
+        for (size_t _ = 0; _ < nCount; _++)
+        {
+            listBuffer.push_front(stkStacks[nFrom - 1].top());
+            stkStacks[nFrom - 1].pop();
+        }
+
+        for (size_t _ = 0; _ < nCount; _++)
+        {
+            stkStacks[nTo - 1].push(listBuffer.front());
+            listBuffer.pop_front();
+        }
+    }
+
+    for (size_t i = 0; i < 9; i++)
+        std::cout << stkStacks[i].top();
+
+    std::cout << std::endl;
+
+    return 0;
+}
