11b0b143a25a5c172399d9751d7f40cf2d89c20d
Алекс <75216201+defini7@users.noreply.github.com>
2022-12-13 19:46:55 +0300

    Create Part1.js

---
+++ day11/Part1.js
@@ -0,0 +1,54 @@
+/** @type { (input: string) => any } */
+let solution = (input) => {
+    let mod = 1
+    const monkeys = []
+    
+    input = input.split('\r\n\r\n')
+
+    console.log(input)
+
+    for (let m of input) {
+        m = m.split('\r\n')
+
+        let num = +m[0].match(/(\d+):$/)[1];
+		let items = m[1].match(/: (.*)$/)[1].split(",").map((a) => +a);
+		let operation = m[2].match(/\= (.*)$/)[1];
+		let test = +m[3].match(/(\d+)$/)[1];
+		let ifTrue = +m[4].match(/(\d+)$/)[1];
+		let ifFalse = +m[5].match(/(\d+)$/)[1];
+        
+		mod *= test;
+		monkeys[num] = { 
+            operation, items, test, ifTrue, ifFalse,
+			inspected: 0
+        };
+    }
+
+    for (let i = 0; i < 20; i++) {
+        for (const m of monkeys) {
+            for (let j = m.items.length - 1; j >= 0; j--) {
+                /*
+                    Quite tricky part,
+                    because firstly we define old, for an expression that
+                    we need to evaluate, and then we call eval function
+                */
+                let old = m.items[j]
+                old = Math.floor(eval(m.operation) / 3)
+
+                monkeys[
+                    (old % m.test === 0) ? 
+                    m.ifTrue :
+                    m.ifFalse
+                ].items.push(old)
+
+                m.items.pop()
+                m.inspected++
+            }
+        }
+    }
+
+    return [...monkeys]
+        .sort((a, b) => b.inspected - a.inspected)
+        .slice(0, 2)
+        .reduce((a, b) => a * b.inspected, 1)
+}
