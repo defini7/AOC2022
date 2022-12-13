1f267840c492793fceacb08263ee62fb7f6b114d
Алекс <75216201+defini7@users.noreply.github.com>
2022-12-13 19:50:42 +0300

    Update Part1.js

--- day11/Part1.js
+++ day11/Part1.js
@@ -10,21 +10,21 @@ let solution = (input) => {
         // if it won't work just remove \r
         m = m.split('\r\n')
 
-        let num = +m[0].match(/(\d+):$/)[1];
-		let items = m[1].match(/: (.*)$/)[1].split(",").map((a) => +a);
-		let operation = m[2].match(/\= (.*)$/)[1];
-		let test = +m[3].match(/(\d+)$/)[1];
-		let ifTrue = +m[4].match(/(\d+)$/)[1];
-		let ifFalse = +m[5].match(/(\d+)$/)[1];
+        let num = +m[0].match(/(\d+):$/)[1]
+		let items = m[1].match(/: (.*)$/)[1].split(",").map((a) => +a)
+		let operation = m[2].match(/\= (.*)$/)[1]
+		let test = +m[3].match(/(\d+)$/)[1]
+		let ifTrue = +m[4].match(/(\d+)$/)[1]
+		let ifFalse = +m[5].match(/(\d+)$/)[1]
         
-		mod *= test;
+		mod *= test
 		monkeys[num] = { 
             operation, items, test, ifTrue, ifFalse,
 			inspected: 0
-        };
+        }
     }
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 10000; i++) {
         for (const m of monkeys) {
             for (let j = m.items.length - 1; j >= 0; j--) {
                 /*
@@ -33,7 +33,7 @@ let solution = (input) => {
                     we need to evaluate, and then we call eval function
                 */
                 let old = m.items[j]
-                old = Math.floor(eval(m.operation) / 3)
+                old = Math.floor(eval(m.operation))
 
                 monkeys[
                     (old % m.test === 0) ? 
